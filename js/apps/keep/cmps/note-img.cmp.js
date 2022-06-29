b2bed9eb9829bd1477a4497df75113b5ccad9eed
Bnaya <bnaya98@gmail.com>
2022-06-29 14:17:34 +0300

    css setup

--- js/apps/keep/cmps/note-img.cmp.js
+++ js/apps/keep/cmps/note-img.cmp.js
@@ -1,24 +1,13 @@
-// import { bookService } from '../services/book-service.js';
-// export default {
-//   template: `<section @click="$emit('select')" class="book-preview">
-//     <img :src="book.thumbnail" alt="" />
-//     <h3>{{book.title}}</h3>
-//     <p>{{showPrice}}</p>
-//   </section>
-//   `,
-//   props: ['book'],
-//   data() {
-//     return {};
-//   },
-//   computed: {
-//     showPriceByCurrency() {
-//       return;
-//     },
-//     showPrice() {
-//       const {
-//         listPrice: { currencyCode, amount },
-//       } = this.book;
-//       return bookService.displayByCurrency(currencyCode).format(amount);
-//     },
-//   },
-// };
+export default {
+  props: ['info'],
+  template: `<article class="note-container">
+    <img :src="info.url" :alt="info.title">
+
+  </article>
+  `,
+  data() {
+    return {};
+  },
+  computed: {
+  },
+};
--- js/apps/keep/cmps/note-todos.cmp.js
+++ js/apps/keep/cmps/note-todos.cmp.js
@@ -1,24 +1,26 @@
-// import { bookService } from '../services/book-service.js';
-// export default {
-//   template: `<section @click="$emit('select')" class="book-preview">
-//     <img :src="book.thumbnail" alt="" />
-//     <h3>{{book.title}}</h3>
-//     <p>{{showPrice}}</p>
-//   </section>
-//   `,
-//   props: ['book'],
-//   data() {
-//     return {};
-//   },
-//   computed: {
-//     showPriceByCurrency() {
-//       return;
-//     },
-//     showPrice() {
-//       const {
-//         listPrice: { currencyCode, amount },
-//       } = this.book;
-//       return bookService.displayByCurrency(currencyCode).format(amount);
-//     },
-//   },
-// };
+export default {
+  props: ['info'],
+  template: `<article class="note-container">
+    <h2>{{info.label}}</h2>
+    <ul>
+      <li v-for="todo in info.todos">
+        <p :style="lineStyle(todo)">{{todo.txt}}</p>
+      </li>
+    </ul>
+
+  </article>
+  `,
+  data() {
+    return {
+      currTodo:null
+    };
+  },
+  methods:{},
+  computed: {
+    lineStyle(todo) {
+      return {
+        textDecoration: todo.doneAt?'line-through':'none'
+      }
+    }
+  },
+};
