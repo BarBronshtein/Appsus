f7e447523a98fb6826cc0f14f24f22d169f491ac
BarBronshtein <barbronshtein@gmail.com>
2022-06-29 20:59:34 +0300

    added feature filter by text

--- js/apps/email/cmps/email-filter.cmp.js
+++ js/apps/email/cmps/email-filter.cmp.js
@@ -1,15 +1,15 @@
 export default {
   template: `
  <section class="email-filter">
-    <input type="search" placeholder="Search mail">
-    <select >
+    <input @input="$emit('filtered',filterBy)" type="search" v-model="filterBy.txt" placeholder="Search mail">
+    <select v-model="filterBy.status" @change="$emit('filtered',filterBy)">
         <option v-for="opt in options" :value="opt">{{opt.replace(opt[0], opt[0].toUpperCase())}}</option>
     </select>
  </section>
 `,
   data() {
     return {
-      txt: '',
+      filterBy: { txt: '', status: 'all' },
       options: ['all', 'read', 'unread', 'draft', 'sent', 'starred'],
     };
   },
