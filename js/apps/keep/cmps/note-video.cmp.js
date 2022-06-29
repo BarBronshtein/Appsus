97ccb2d80938c80d8d457f3b4e54d10753ad4bdc
Bnaya <bnaya98@gmail.com>
2022-06-29 21:41:08 +0300

    image feature working

--- js/apps/keep/cmps/note-video.cmp.js
+++ js/apps/keep/cmps/note-video.cmp.js
@@ -4,7 +4,7 @@ export default {
     props: ['note'],
     template: `<article class="note-container">
       <h3>{{note.info.title}}</h3>
-        <iframe class="video-player"  width="350" height="200"
+        <iframe class="video-player"  width="100%" height="200"
         :src="note.info.url">
         </iframe>    
         <note-actions :note="note"/>          
