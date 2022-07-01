881596b722e90af503bd1bee8bac4fa12547536f
BarBronshtein <barbronsthein@gmail.com>
2022-07-01 14:02:29 +0300

    added preview styling

--- js/apps/email/cmps/email-list.cmp.js
+++ js/apps/email/cmps/email-list.cmp.js
@@ -3,10 +3,8 @@ export default {
   template: `
   <section class="email-list">
   <ul class="clean-list">
-    <li v-for="email in emails" :key="email.id" class="email-preview-container">
-      
-        <email-preview @reply="replyEmail" @selectedEmail="goToLink" :selectedEmail="selectedEmail" :email="email"/>
-      
+    <li v-for="email in emails" :key="email.id" :class="[email.isRead ? 'email-read' : '','email-preview-container']">
+      <email-preview @reply="replyEmail" @selectedEmail="goToLink" :selectedEmail="selectedEmail" :email="email"/>
     </li>
   </ul>
   </section>
