a2d4bf8068ea9421d128844626e70fcdcd700839
Bnaya <bnaya98@gmail.com>
2022-07-02 19:50:48 +0300

    keep complete

--- js/views/home-page.cmp.js
+++ js/views/home-page.cmp.js
@@ -1,7 +1,12 @@
 export default {
   template: `
  <section class="home-page app-main">
-    <h3>Home sweet home</h3>
+    <h3>Choose your App</h3>
+    <section class="">
+      <div>Books</div>
+      <div>Email</div>
+      <div>Keep</div>
+    </section>
  </section>
 `,
   data() {
