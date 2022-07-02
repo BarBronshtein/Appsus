466f9b01a18d2b85672a777338adc896db7c6d86
BarBronshtein <barbronshtein@gmail.com>
2022-07-02 10:55:08 +0300

    added hamburger menu to header

--- js/cmps/app-header.cmp.js
+++ js/cmps/app-header.cmp.js
@@ -1,16 +1,41 @@
 export default {
   template: `
+      <div @click="toggleMenu" :class="[menuOpen,'main-screen']"></div>
      <header class="app-header">
     <div class="logo">
         <h3 class="fa-brands fa-accusoft"> Books!</h3>
     </div>
-      <ul class="main-navbar">
-        <router-link router-link to="/">Home</router-link>
-        <router-link router-link to="/email/inbox">Email</router-link>
-        <router-link router-link to="/keep">Keep</router-link>
-        <router-link router-link to="/book">Books</router-link>
-        <router-link router-link to="/about">About</router-link>
+      <ul :class="[expand,'main-navbar']">
+        <router-link @click="toggleMenu(false)" :to="sendTo(opt)" v-for="(opt,i) in options" :key="opt">{{opt.replace(opt[0],opt[0].toUpperCase())}}</router-link>
       </ul>
+      <button @click="toggleMenu" class="btn menu-toggle">{{menuSign}}</button>
     </header>
  `,
+  data() {
+    return {
+      options: ['home', 'email', 'keep', 'book', 'about'],
+      isOpen: false,
+    };
+  },
+  methods: {
+    toggleMenu() {
+      this.isOpen = !this.isOpen;
+    },
+    sendTo(opt) {
+      if (opt === 'email') return '/email/inbox';
+      if (opt === 'home') return '/';
+      return '/' + opt;
+    },
+  },
+  computed: {
+    menuSign() {
+      return this.isOpen ? 'X' : '☰';
+    },
+    menuOpen() {
+      return this.isOpen ? 'menu-open' : '';
+    },
+    expand() {
+      if (this.isOpen) return 'expand';
+    },
+  },
 };
